beb9c0cb288b0d042ba49907c67426efaabfbb80
fenet roba <fenetroba700@gmail.com>
2025-05-30 11:40:44 +0300

    fd

--- back_end/routes/prescriptionRoutes.js
+++ back_end/routes/prescriptionRoutes.js
@@ -7,7 +7,7 @@ const router =express.Router();
 router.post('/create_prescription',authMiddleware,CreatePrescription) 
 router.get('/all_prescription',authMiddleware,GetAllPrescriptions)
 router.get('/single_prescription/:patientName',authMiddleware,fetch_byPatient_name)
-router.patch('/update_prescription/:id',authMiddleware,UpdatePrescription)
+router.put('/update_prescription/:id',authMiddleware,UpdatePrescription)
 router.delete('/delete_prescription/:id',authMiddleware,DeletePrescription)
 
 export default router
